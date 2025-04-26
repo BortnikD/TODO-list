7ecaf5598608ea88b8173dead73f7e1699a31ce4
dmitrii bortnik <dima100421@gmail.com>
2025-04-26 16:23:29 +0300

    feat(infrastructure): Created exposed entities

---
+++ src/main/kotlin/com/bortnik/todo/infrastructure/persistence/entities/CategoryEntity.kt
@@ -0,0 +1,13 @@
+package com.bortnik.todo.infrastructure.persistence.entities
+
+import com.bortnik.todo.infrastructure.persistence.tables.CategoriesTable
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+
+class CategoryEntity(id: EntityID<Int>): IntEntity(id) {
+
+    companion object : IntEntityClass<CategoryEntity>(CategoriesTable)
+
+    var name by CategoriesTable.name
+}
---
+++ src/main/kotlin/com/bortnik/todo/infrastructure/persistence/entities/TaskEntity.kt
@@ -0,0 +1,18 @@
+package com.bortnik.todo.infrastructure.persistence.entities
+
+import com.bortnik.todo.infrastructure.persistence.tables.TasksTable
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+
+class TaskEntity(id: EntityID<Int>): IntEntity(id) {
+
+    companion object : IntEntityClass<TaskEntity>(TasksTable)
+
+    var categoryId by TasksTable.categoryId
+    var priority by TasksTable.priority
+    var text by TasksTable.text
+    var createdAt by TasksTable.createdAt
+    var isCompleted by TasksTable.isCompleted
+    var completedAt by TasksTable.completedAt
+}

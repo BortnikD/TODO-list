305ead53bb5644c4efeb6b08d87725f4692bfa76
dmitrii bortnik <dima100421@gmail.com>
2025-04-27 12:18:44 +0300

    feat(api): created controllers

---
+++ src/main/kotlin/com/bortnik/todo/api/http/controllers/CategoryController.kt
@@ -0,0 +1,39 @@
+package com.bortnik.todo.api.http.controllers
+
+import com.bortnik.todo.domain.dto.CategoryCreate
+import com.bortnik.todo.domain.entities.Category
+import com.bortnik.todo.domain.exceptions.CategoryNotFound
+import com.bortnik.todo.usecase.category.CreateCategoryUseCase
+import com.bortnik.todo.usecase.category.DeleteCategoryUseCase
+import com.bortnik.todo.usecase.category.GetCategoryUseCase
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/categories")
+class CategoryController(
+    private val createCategoryUseCase: CreateCategoryUseCase,
+    private val deleteCategoryUseCase: DeleteCategoryUseCase,
+    private val getCategoryUseCase: GetCategoryUseCase
+) {
+
+    @PostMapping
+    fun addCategory(@RequestBody category: CategoryCreate): Category {
+        return createCategoryUseCase.addCategory(category)
+    }
+
+    @DeleteMapping("/{categoryId}")
+    fun deleteCategory(@PathVariable categoryId: Int) {
+        deleteCategoryUseCase.deleteCategory(categoryId)
+    }
+
+    @GetMapping("/{categoryId}")
+    fun getCategoryById(@PathVariable categoryId: Int): Category {
+        return getCategoryUseCase.getCategoryById(categoryId) ?: throw CategoryNotFound("category not found")
+    }
+}
---
+++ src/main/kotlin/com/bortnik/todo/api/http/controllers/TaskController.kt
@@ -0,0 +1,51 @@
+package com.bortnik.todo.api.http.controllers
+
+import com.bortnik.todo.domain.dto.TaskCreate
+import com.bortnik.todo.domain.dto.TaskUpdate
+import com.bortnik.todo.domain.entities.Task
+import com.bortnik.todo.usecase.task.CreateTaskUseCase
+import com.bortnik.todo.usecase.task.GetTaskUseCase
+import com.bortnik.todo.usecase.task.UpdateTaskUseCase
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/api/tasks")
+class TaskController(
+    private val createTaskUseCase: CreateTaskUseCase,
+    private val getTaskUseCase: GetTaskUseCase,
+    private val updateTaskUseCase: UpdateTaskUseCase
+) {
+
+    @GetMapping()
+    fun getUncompletedTasks(@RequestParam field: String = "priority"): List<Task> {
+        return getTaskUseCase.getTasksSortedByFieldOrDefault(field)
+    }
+
+    @GetMapping("/completed")
+    fun getCompletedTasks(@RequestParam field: String = "priority"): List<Task> {
+        return getTaskUseCase.getCompletedTasksSortedByFieldOrDefault(field)
+    }
+
+    @PostMapping()
+    fun addTask(@RequestBody task: TaskCreate): Task {
+        return createTaskUseCase.addTask(task)
+    }
+
+    @PatchMapping()
+    fun updateTask(@RequestBody task: TaskUpdate): Task {
+        return updateTaskUseCase.updateTask(task)
+    }
+
+    @PatchMapping("/complete/{taskId}")
+    fun completeTask(@PathVariable taskId: Int) {
+        return updateTaskUseCase.completeTask(taskId)
+    }
+}

9ecb91a3bf997e1343f4f70aa31aeb0369c0d096
dmitrii bortnik <dima100421@gmail.com>
2025-04-30 18:17:53 +0300

    refactor(domain): return entities, now dtos

--- src/main/kotlin/com/bortnik/todo/domain/repositories/UserRepository.kt
+++ src/main/kotlin/com/bortnik/todo/domain/repositories/UserRepository.kt
@@ -1,29 +1,29 @@
 package com.bortnik.todo.domain.repositories
 
 import com.bortnik.todo.domain.dto.UserCreate
-import com.bortnik.todo.domain.dto.UserPublic
+import com.bortnik.todo.domain.entities.User
 
 interface UserRepository {
 
     /**
      * Create new user
      */
-    fun save(user: UserCreate): UserPublic
+    fun save(user: UserCreate): User
 
     /**
      * get user by user id
      */
-    fun getById(userId: Int): UserPublic
+    fun getById(userId: Int): User
 
     /**
      * get user by username
      */
-    fun getByUsername(username: String): UserPublic
+    fun getByUsername(username: String): User
 
     /**
      * get user by email
      */
-    fun getByEmail(email: String): UserPublic
+    fun getByEmail(email: String): User
 
     /**
      * delete user by user id

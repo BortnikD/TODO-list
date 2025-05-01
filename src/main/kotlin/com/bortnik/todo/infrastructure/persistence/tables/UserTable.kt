fd06584cfe357aaa0ecec55f7b06ecc98ee92bc7
dmitrii bortnik <dima100421@gmail.com>
2025-05-01 12:03:29 +0300

    feat(infrastructure.persistence): created user table

---
+++ src/main/kotlin/com/bortnik/todo/infrastructure/persistence/tables/UserTable.kt
@@ -0,0 +1,13 @@
+package com.bortnik.todo.infrastructure.persistence.tables
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
+import org.jetbrains.exposed.sql.javatime.datetime
+
+object UserTable: IntIdTable("users") {
+    val username = varchar("username", 64)
+    val email = varchar("email", 64)
+    val hashedPassword = varchar("hashed_password", 256)
+    val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
+    val updatedAt = datetime("updated_at").defaultExpression(CurrentDateTime)
+}

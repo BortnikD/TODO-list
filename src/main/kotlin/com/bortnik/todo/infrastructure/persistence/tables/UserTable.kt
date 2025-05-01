f97f39b4189fa2f02326717b72fa6727a99f92d2
dmitrii bortnik <dima100421@gmail.com>
2025-05-01 12:46:16 +0300

    feat(infrastructure.persistence): create user repository

--- src/main/kotlin/com/bortnik/todo/infrastructure/persistence/tables/UserTable.kt
+++ src/main/kotlin/com/bortnik/todo/infrastructure/persistence/tables/UserTable.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.sql.javatime.datetime
 
 object UserTable: IntIdTable("users") {
     val username = varchar("username", 64)
-    val email = varchar("email", 64)
+    val email = varchar("email", 64).nullable()
     val hashedPassword = varchar("hashed_password", 256)
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
     val updatedAt = datetime("updated_at").defaultExpression(CurrentDateTime)

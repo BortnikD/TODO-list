584eea00ec3e056314d2c210e2775ffe627da33a
dmitrii bortnik <dima100421@gmail.com>
2025-05-01 13:00:00 +0300

    refactor(db): fix migration

--- src/main/resources/db/migration/V5__add_users_references_to_tasks_and_categories.sql
+++ src/main/resources/db/migration/V5__add_users_references_to_tasks_and_categories.sql
@@ -2,18 +2,6 @@
 ALTER TABLE tasks
 ADD COLUMN userId INTEGER REFERENCES users(id);
 
-UPDATE tasks
-SET userId = 1
-where userId IS NULL;
-
-ALTER TABLE tasks ALTER COLUMN userId SET NOT NULL;
-
 -- Add userId to categories
 ALTER TABLE categories
-    ADD COLUMN userId INTEGER REFERENCES users(id);
-
-UPDATE categories
-SET userId = 1
-where userId IS NULL;
-
-ALTER TABLE categories ALTER COLUMN userId SET NOT NULL;
+ADD COLUMN userId INTEGER REFERENCES users(id);

b4bbc7df2b2d3e926b3a2679032c0df3567b0834
dmitrii bortnik <dima100421@gmail.com>
2025-05-04 13:19:23 +0300

    feat(infrastructure): created UserDetailsServiceImpl

---
+++ src/main/kotlin/com/bortnik/todo/infrastructure/security/UserDetailsServiceImpl.kt
@@ -0,0 +1,26 @@
+package com.bortnik.todo.infrastructure.security
+
+import com.bortnik.todo.domain.exceptions.user.UserNotFound
+import com.bortnik.todo.domain.repositories.UserRepository
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+
+@Service
+class UserDetailsServiceImpl(
+    private val userRepository: UserRepository
+): UserDetailsService {
+
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.getByUsername(username)
+            ?: throw UserNotFound("User not found with username: $username")
+
+        return User.builder()
+            .username(user.username)
+            .password(user.password)
+            .authorities(SimpleGrantedAuthority("USER"))
+            .build()
+    }
+}

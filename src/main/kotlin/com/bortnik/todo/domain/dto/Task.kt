8cd652eaa07d380d4647428280037f448fa560b8
dmitrii bortnik <dima100421@gmail.com>
2025-05-06 11:32:32 +0300

    feat(domain.dto.Task): added TaskCreateRequest

--- src/main/kotlin/com/bortnik/todo/domain/dto/Task.kt
+++ src/main/kotlin/com/bortnik/todo/domain/dto/Task.kt
@@ -7,14 +7,15 @@ data class TaskCreate(
     val text: String,
 )
 
+data class TaskCreateRequest (
+    val categoryId: Int,
+    val priority: Int = 1,
+    val text: String,
+)
+
 data class TaskUpdate(
     val id: Int,
     val categoryId: Int,
     val priority: Int,
     val text: String
-)
-
-data class TaskComplete(
-    val id: Int,
-    val userId: Int
 )

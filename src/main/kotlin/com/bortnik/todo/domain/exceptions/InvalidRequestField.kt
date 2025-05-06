27f3423de64c707a6a8f97b2c517ef94142de93d
dmitrii bortnik <dima100421@gmail.com>
2025-05-06 23:31:58 +0300

    fixed(exceptions.InvalidRequestFiled): added message to super in second constructor

--- src/main/kotlin/com/bortnik/todo/domain/exceptions/InvalidRequestField.kt
+++ src/main/kotlin/com/bortnik/todo/domain/exceptions/InvalidRequestField.kt
@@ -2,5 +2,5 @@ package com.bortnik.todo.domain.exceptions
 
 class InvalidRequestField: Exception {
     constructor(message: String): super(message)
-    constructor(message: String, cause: Throwable): super(cause)
+    constructor(message: String, cause: Throwable): super(message, cause)
 }

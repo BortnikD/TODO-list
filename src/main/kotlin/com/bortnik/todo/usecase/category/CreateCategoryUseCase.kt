edcf9f9179409bbd74f78d28c6f88047981e7e1b
dmitrii bortnik <dima100421@gmail.com>
2025-05-10 09:12:36 +0300

    feat(usecase): added category already exists check to add category method

--- src/main/kotlin/com/bortnik/todo/usecase/category/CreateCategoryUseCase.kt
+++ src/main/kotlin/com/bortnik/todo/usecase/category/CreateCategoryUseCase.kt
@@ -2,6 +2,7 @@ package com.bortnik.todo.usecase.category
 
 import com.bortnik.todo.domain.dto.CategoryCreate
 import com.bortnik.todo.domain.entities.Category
+import com.bortnik.todo.domain.exceptions.category.CategoryAlreadyExists
 import com.bortnik.todo.domain.repositories.CategoryRepository
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.stereotype.Service
@@ -13,6 +14,9 @@ class CreateCategoryUseCase(
 
     @CacheEvict(value = ["categories.byUserId"])
     fun addCategory(category: CategoryCreate): Category {
+        if (categoryRepository.getCategoryByUserIdAndName(category.userId, category.name) != null) {
+            throw CategoryAlreadyExists("category with name '${category.name}' already exists")
+        }
         return categoryRepository.addCategory(category)
     }
 }

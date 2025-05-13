099b30d1333a5a31fb7bfc52356dd63c64c58709
dmitrii bortnik <dima100421@gmail.com>
2025-05-13 16:05:03 +0300

    test(AuthController): created tests for auth controller

---
+++ src/test/kotlin/com/bortnik/todo/api/AuthControllerTests.kt
@@ -0,0 +1,128 @@
+package com.bortnik.todo.api
+
+import com.bortnik.todo.api.http.controllers.AuthController
+import com.bortnik.todo.api.http.exceptions.BadCredentials
+import com.bortnik.todo.domain.dto.user.AuthResponse
+import com.bortnik.todo.domain.dto.user.UserCreate
+import com.bortnik.todo.domain.dto.user.UserLogin
+import com.bortnik.todo.usecase.AuthService
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class AuthControllerTests {
+
+    private val authService = mockk<AuthService>()
+    private val controller = AuthController(authService)
+
+    private val validUserCreate = UserCreate(
+        username = "username",
+        password = "password123",
+        email = "test@example.com"
+    )
+
+    private val validUserLogin = UserLogin(
+        username = "username",
+        password = "password123"
+    )
+
+    private val authResponse = AuthResponse(
+        token = "test-token",
+        username = "username"
+    )
+
+    @Test
+    fun `register should throws BadCredentials when username length is incorrect`(){
+        val user = validUserCreate.copy(username = "us")
+
+        val exception = assertThrows<BadCredentials> {
+            controller.register(user)
+        }
+
+        assertEquals("username is too long or short", exception.message)
+    }
+
+    @Test
+    fun `register should throws BadCredentials when username is not email and contains wrong characters`() {
+        // username contains '-' is not correct
+        val user = validUserCreate.copy(username = "general-Kenobi")
+
+        val exception = assertThrows<BadCredentials> {
+            controller.register(user)
+        }
+
+        assertEquals("username can contains only latin characters, digits, and '_'", exception.message)
+    }
+
+    @Test
+    fun `register should throws BadCredentials when password length less than 8`() {
+        val user = validUserCreate.copy(password = "1234567")
+
+        val exception = assertThrows<BadCredentials> {
+            controller.register(user)
+        }
+
+        assertEquals("password length must be greater or equal to 8", exception.message)
+    }
+
+    @Test
+    fun `register should throws BadCredentials when email is incorrect`() {
+        listOf(
+            validUserCreate.copy(email = "wrongEmail"),
+            validUserCreate.copy(email = "wrong@email"),
+            validUserCreate.copy(email = "wrong@email."),
+            validUserCreate.copy(email = "@email.com")
+        ).forEach { user ->
+            val exception = assertThrows<BadCredentials> { controller.register(user) }
+            assertEquals("incorrect email", exception.message)
+        }
+    }
+
+    @Test
+    fun `register should return auth response when credentials are valid`() {
+        every { authService.register(validUserCreate) } returns authResponse
+        assertEquals(authResponse, controller.register(validUserCreate))
+    }
+
+    @Test
+    fun `authenticate should throws BadCredentials when username length is incorrect`(){
+        val user = validUserLogin.copy(username = "us")
+
+        val exception = assertThrows<BadCredentials> {
+            controller.authenticate(user)
+        }
+
+        assertEquals("username is too long or short", exception.message)
+    }
+
+    @Test
+    fun `authenticate should throws BadCredentials when username is not email and contains wrong characters`() {
+        // username contains '-' is not correct
+        val user = validUserLogin.copy(username = "general-Kenobi")
+
+        val exception = assertThrows<BadCredentials> {
+            controller.authenticate(user)
+        }
+
+        assertEquals("username can contains only latin characters, digits, and '_'", exception.message)
+    }
+
+    @Test
+    fun `authenticate should throws BadCredentials when password length less than 8`() {
+        val user = validUserLogin.copy(password = "1234567")
+
+        val exception = assertThrows<BadCredentials> {
+            controller.authenticate(user)
+        }
+
+        assertEquals("password length must be greater or equal to 8", exception.message)
+    }
+
+    @Test
+    fun `authenticate should return auth response when credentials are valid`() {
+        every { authService.authentication(validUserLogin) } returns authResponse
+        assertEquals(authResponse, controller.authenticate(validUserLogin))
+    }
+}

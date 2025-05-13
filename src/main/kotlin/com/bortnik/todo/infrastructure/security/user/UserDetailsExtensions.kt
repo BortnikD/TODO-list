fce4f4ba9db1152693e4d3260ceef2007f9713d4
dmitrii bortnik <dima100421@gmail.com>
2025-05-13 17:10:59 +0300

    feat(UserDetailsExtensions): added function to get domain user

--- src/main/kotlin/com/bortnik/todo/infrastructure/security/user/UserDetailsExtensions.kt
+++ src/main/kotlin/com/bortnik/todo/infrastructure/security/user/UserDetailsExtensions.kt
@@ -1,10 +1,16 @@
 package com.bortnik.todo.infrastructure.security.user
 
+import com.bortnik.todo.domain.entities.User
 import com.bortnik.todo.domain.exceptions.user.UserNotFound
 import com.bortnik.todo.usecase.user.GetUserUseCase
 import org.springframework.security.core.userdetails.UserDetails
 
 fun UserDetails.getUserId(getUserUseCase: GetUserUseCase): Int {
     return getUserUseCase.getByUsername(this.username)?.id
+        ?: throw UserNotFound("Not found user with username '${this.username}'")
+}
+
+fun UserDetails.getDomainUser(getUserUseCase: GetUserUseCase): User {
+    return getUserUseCase.getByUsername(this.username)
         ?: throw UserNotFound("Not found user with username '${this.username}'")
 }
